74cac91ac31ed565931fcdc9d2af9520a720c6fb
Juan <jalbarracin@een.com>
2021-04-23 20:46:43 +0200

    test 1

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,4 +1,26 @@
+import java.io.File
+import java.util.concurrent.TimeUnit
 
 fun main(args: Array<String>){
     println("Hello kotlin")
 }
+
+class Main {
+    fun test1() {
+        println("This is a test")
+    }
+}
+
+fun test2() {
+    println("This is a test 2")
+}
+
+
+fun String.runCommand(workingDir: File = File("."), timeoutAmount: Long = 60, timeoutUnit: TimeUnit = TimeUnit.SECONDS): String? = runCatching {
+    ProcessBuilder("\\s".toRegex().split(this))
+        .directory(workingDir)
+        .redirectOutput(ProcessBuilder.Redirect.PIPE)
+        .redirectError(ProcessBuilder.Redirect.PIPE)
+        .start().also { it.waitFor(timeoutAmount, timeoutUnit) }
+        .inputStream.bufferedReader().readText()
+}.onFailure { it.printStackTrace() }.getOrNull()
